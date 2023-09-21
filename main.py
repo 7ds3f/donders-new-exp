31d9b1a83d2440e0adb33a2255590ec53786f624
Will Bordman <willbordman@pm.me>
2023-09-21 14:13:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -44,57 +44,11 @@ def clear():
         _ = system('clear')
 
 #simple reaction time test
-def simple(count):
+def simple(max_delay, count):
     times = []
     #measure time required for long keystrokes
     print("[ Simple Reaction Time Test ]")
-    print("When prompted, press \"1\" and then \"0\" as fast as you can using ONE FINGER")
-    time.sleep(5)
-    for itr in range(count):
-        print(f"Test {itr+1}/{count}")
-        val1 = "-1"
-        val2 = "-1"
-        end_time = 0
-        while (val2 != "0"):
-            time.sleep(1)
-            print(f"Press \"1\" and then \"0\"!")
-            start_time = time.time()
-            val1 = getch()
-            val2 = getch()
-            end_time = time.time() - start_time
-            if val1 != "1":
-                print("Wrong input! Try again...")
-                val2 = "-1"
-            elif val2 != "0":
-                print("Wrong input! Try again...")
-        times.append(end_time)
-    #measure time required for medium keystrokes
-    clear()
-    print("[ Simple Reaction Time Test ]")
-    print("When prompted, press \"3\" and then \"8\" as fast as you can using ONE FINGER")
-    time.sleep(5)
-    for itr in range(count):
-        print(f"Test {itr+1}/{count}")
-        val1 = "-1"
-        val2 = "-1"
-        end_time = 0
-        while (val2 != "8"):
-            time.sleep(1)
-            print(f"Press \"3\" and then \"8\"!")
-            start_time = time.time()
-            val1 = getch()
-            val2 = getch()
-            end_time = time.time() - start_time
-            if val1 != "3":
-                print("Wrong input! Try again...")
-                val2 = "-1"
-            elif val2 != "8":
-                print("Wrong input! Try again...")
-        times.append(end_time)
-    #measure time required for short keystrokes
-    clear()
-    print("[ Simple Reaction Time Test ]")
-    print("When prompted, press \"5\" and then \"6\" as fast as you can using ONE FINGER")
+    print("When prompted, press \"[SPACE]\" and then \"6\" as fast as you can using ONE FINGER")
     time.sleep(5)
     for itr in range(count):
         print(f"Test {itr+1}/{count}")
@@ -102,16 +56,39 @@ def simple(count):
         val2 = "-1"
         end_time = 0
         while (val2 != "6"):
-            time.sleep(1)
-            print(f"Press \"5\" and then \"6\"!")
+            time.sleep(random.randint(1,max_delay))
+            print(f"Press \"[SPACE]\" and then \"6\"!")
             start_time = time.time()
             val1 = getch()
             val2 = getch()
             end_time = time.time() - start_time
-            if val1 != "5":
+            if not str.isspace(val1):
                 print("Wrong input! Try again...")
                 val2 = "-1"
             elif val2 != "6":
+                print("Wrong input! Try again...")
+        times.append(end_time)
+    #measure time required for medium keystrokes
+    clear()
+    print("[ Simple Reaction Time Test ]")
+    print("When prompted, press \"[SPACE]\" and then \"y\" as fast as you can using ONE FINGER")
+    time.sleep(5)
+    for itr in range(count):
+        print(f"Test {itr+1}/{count}")
+        val1 = "-1"
+        val2 = "-1"
+        end_time = 0
+        while (val2 != "y"):
+            time.sleep(random.randint(1,max_delay))
+            print(f"Press \"[SPACE]\" and then \"y\"!")
+            start_time = time.time()
+            val1 = getch()
+            val2 = getch()
+            end_time = time.time() - start_time
+            if not str.isspace(val1):
+                print("Wrong input! Try again...")
+                val2 = "-1"
+            elif val2 != "y":
                 print("Wrong input! Try again...")
         times.append(end_time)
     return times
@@ -119,45 +96,55 @@ def simple(count):
 #number choice reaction time test
 def number_choice(max_delay, count, numbers):
     times = []
-    print("[ Choice Reaction Time Test (NUMBERS) ]")
-    print(f"Press the displayed NUMBER as fast as you can...")
+    #measure time required for long keystrokes
+    print("[ Simple Reaction Time Test ]")
+    print("When prompted, press \"[SPACE]\" and then the displayed NUMBER as fast as you can using ONE FINGER")
     time.sleep(5)
     for itr in range(count):
         number = random.choice(numbers)
         print(f"Test {itr+1}/{count}")
-        val = -1
+        val1 = "-1"
+        val2 = "-1"
         end_time = 0
-        while (val != number):
+        while (val2 != str(number)):
             time.sleep(random.randint(1,max_delay))
-            print(f"Press {number}")
+            print(f"Press \"[SPACE]\" and then {number}!")
             start_time = time.time()
-            val = getch()
+            val1 = getch()
+            val2 = getch()
             end_time = time.time() - start_time
-            if val != str(number):
+            if not str.isspace(val1):
                 print("Wrong input! Try again...")
-            else:
-                val = int(val)
+                val2 = "-1"
+            elif val2 != str(number):
+                print("Wrong input! Try again...")
         times.append(end_time)
     return times
 
 #letter choice reaction time test
 def letter_choice(max_delay, count, letters):
     times = []
-    print("[ Choice Reaction Time Test (LETTERS) ]")
-    print(f"Press the displayed LETTER as fast as you can...")
+    #measure time required for long keystrokes
+    print("[ Simple Reaction Time Test ]")
+    print("When prompted, press \"[SPACE]\" and then the displayed LETTER as fast as you can using ONE FINGER")
     time.sleep(5)
     for itr in range(count):
         letter = random.choice(letters)
         print(f"Test {itr+1}/{count}")
-        val = -1
+        val1 = "-1"
+        val2 = "-1"
         end_time = 0
-        while (val != letter):
+        while (val2 != letter):
             time.sleep(random.randint(1,max_delay))
-            print(f"Press {letter}")
+            print(f"Press \"[SPACE]\" and then {letter}!")
             start_time = time.time()
-            val = getch()
+            val1 = getch()
+            val2 = getch()
             end_time = time.time() - start_time
-            if val != letter:
+            if not str.isspace(val1):
+                print("Wrong input! Try again...")
+                val2 = "-1"
+            elif val2 != letter:
                 print("Wrong input! Try again...")
         times.append(end_time)
     return times
@@ -176,7 +163,7 @@ if __name__ == "__main__":
     clear()
 
     #run simple reaction time test SIMPLE_TESTS_COUNT times
-    simple_tests_results = simple(SIMPLE_TESTS_COUNT)
+    simple_tests_results = simple(MAX_WAIT, SIMPLE_TESTS_COUNT)
     clear()
     #run number choice reaction time test CHOICE_TESTS_COUNT times
     number_choice_tests_results = number_choice(MAX_WAIT, CHOICE_TESTS_COUNT, NUMBERS)
@@ -187,16 +174,18 @@ if __name__ == "__main__":
 
     #calculations
     avg_keystroke_time = sum(simple_tests_results)/len(simple_tests_results)
-    temp1 = [entry - avg_keystroke_time for entry in number_choice_tests_results]
-    temp2 = [entry - avg_keystroke_time for entry in letter_choice_tests_results]
-    number_choice_tests_results = temp1
-    letter_choice_tests_results = temp2
+    #temp1 = [entry - avg_keystroke_time for entry in number_choice_tests_results]
+    #temp2 = [entry - avg_keystroke_time for entry in letter_choice_tests_results]
+    #number_choice_tests_results = temp1
+    #letter_choice_tests_results = temp2
     #for entry in number_choice_tests_results:
     #    entry -= avg_keystroke_time
     #for entry in letter_choice_tests_results:
     #    entry -= avg_keystroke_time
     avg_number_choice_time = sum(number_choice_tests_results)/len(number_choice_tests_results)
     avg_letter_choice_time = sum(letter_choice_tests_results)/len(letter_choice_tests_results)
+    avg_number_desc = avg_number_choice_time - avg_keystroke_time
+    avg_letter_desc = avg_letter_choice_time - avg_keystroke_time
 
     #print calculations
     #simple reaction time
@@ -205,26 +194,28 @@ if __name__ == "__main__":
     print("Keystroke times:")
     for entry in simple_tests_results:
         print("{:.4f}".format(entry))
-    print("Avg:", avg_keystroke_time, "\n")
+    print("Avg Reaction Time:", avg_keystroke_time, "\n")
     #numbers reaction time
     print("[Number Choice Reaction Test]")
     print("Choice times:")
     for entry in number_choice_tests_results:
         print("{:.4f}".format(entry))
-    print("Avg:", avg_number_choice_time, "\n")
+    print("Avg Reaction Time:", avg_number_choice_time)
+    print("Avg Decision Time:", avg_number_desc, "\n")
     #letters reaction time
     print("[Letter Choice Reaction Test]")
     print("Choice times:")
     for entry in letter_choice_tests_results:
         print("{:.4f}".format(entry))
-    print("Avg:", avg_letter_choice_time, "\n")
+    print("Avg Reaction Time:", avg_letter_choice_time)
+    print("Avg Decision Time:", avg_letter_desc, "\n")
     #conclusions
-    if avg_number_choice_time < avg_letter_choice_time:
+    if avg_number_desc < avg_letter_desc:
         print("Decision time for NUMBERS was less than LETTERS")
-        print("Diff:", abs(avg_number_choice_time-avg_letter_choice_time))
-    elif avg_number_choice_time > avg_letter_choice_time:
+        print("Diff:", abs(avg_number_desc-avg_letter_desc))
+    elif avg_number_desc > avg_letter_desc:
         print("Decision time for LETTERS was less than NUMBERS")
-        print("Diff:", abs(avg_number_choice_time-avg_letter_choice_time))
+        print("Diff:", abs(avg_number_desc-avg_letter_desc))
     else:
         print("Decision time for LETTERS and NUMBERS was the same")
         print("Diff:", 0)
